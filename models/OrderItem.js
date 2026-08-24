886a94c76f11b526fbbf6bdb6567660a66fc8260
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-08-24 15:36:53 +0300

    orderItem model

---
+++ models/OrderItem.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    variantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductVariant",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const OrderItem = mongoose.model("OrderItem", orderItemSchema);
+
+module.exports = OrderItem;
