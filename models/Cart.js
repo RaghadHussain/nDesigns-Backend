f297ad0cf06119cf2ef127bd9788b75bc117b282
Bushra-11 <bu2004hu@gmail.com>
2026-08-25 19:57:25 +0300

    fix: ensure userId reference is required in cart schema

--- models/Cart.js
+++ models/Cart.js
@@ -4,7 +4,8 @@ const cartSchema = new mongoose.Schema(
     {
         userId: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
+            ref: 'User',
+            required: true
         }
     },
     { timestamps: true },
